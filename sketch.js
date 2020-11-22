cfc1756c8fac0bf0d0de0c3b6042b37c4546d580
Tanisi24 <71957811+Tanisi24@users.noreply.github.com>
2020-11-22 11:48:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var side1,side2,side3;
+var ground1;
+var paper1;
+
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	side1 = new Dustbin(400,680,200,20);
+	side2 = new Dustbin(300,630,20,100);
+	side3 = new Dustbin(500,630,20,100);
+	ground1 = new Ground(400,690,800,10);
+	paper1 = new Paper(150,380,40);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  side1.display();
+  side2.display();
+  side3.display();
+  ground1.display();
+  paper1.display();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if(keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85,y:-95});
+	}
+}
+
